fc113c6db69ce78ef6647cc9db681cc10da34ea3
Sheefhuss <sheefahussain029@gmail.com>
2026-07-12 14:09:36 +0530

    Add connection and greeting timeouts to mailer

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -7,6 +7,8 @@ const transporter = nodemailer.createTransport({
   port,
   secure: port === 465,
   family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
   auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
 });
 
