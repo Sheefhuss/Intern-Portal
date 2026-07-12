186de9273eee7e81d663ad76ddda88556dea7a26
Sheefhuss <sheefhussain029@gmail.com>
2026-07-12 12:53:12 +0530

    fixing mailer port

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer');
 
+const port = Number(process.env.EMAIL_PORT) || 587;
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 587,
-  secure: false,
+  port,
+  secure: port === 465, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
